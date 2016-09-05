c0c7ab25af2c35f8c9bcdbbc3a567a52a7041eee
Marc Cull <marc@anyroad.com>
2016-09-05 14:25:27 -0700

    first solution!

--- problem_1.js
+++ problem_1.js
@@ -1,1 +1,17 @@
-//first check-in
+var part1 = 0;
+var part2 = 0;
+var part3 = 0;
+for (var i = 1; i <= 333; i++) {
+  part1 += i*3;
+}
+
+
+for (var i = 1; i <= 199; i++) {
+  part2 += i*5;
+}
+
+for (var i = 1; i <= Math.floor(1000/15); i++) {
+  part3 += i*15;
+}
+var sum = part1 + part2 - part3;
+console.log("answer = " + sum);
